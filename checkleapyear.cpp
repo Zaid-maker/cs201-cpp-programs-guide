14a1747b1dac2a878c0d913ab872485b92b06fd2
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2022-05-31 15:46:23 +0500

    new program

---
+++ checkleapyear.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int year;
+    cout << "Enter a year: ";
+    cin >> year;
+
+    // Leap year if perfectly divisible by 400
+    if (year % 400 == 0)
+    {
+        cout << year << " is a leap year." << endl;
+    }
+
+    // not a leap year if divisible by 100
+    // but not divisible by 400
+    else if (year % 100 == 0)
+    {
+        cout << year << " is not a leap year.";
+    }
+
+    // leap yar if not divisible by 100
+    // but divisible by 4
+    else if (year % 4 == 0)
+    {
+        cout << year << " is a leap year.";
+    }
+
+    // all other years are not leap years
+    else
+    {
+        cout << year << " is not a leap year.";
+    }
+
+    return 0;
+}
