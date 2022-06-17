3c554dd07907e640b4280a6c407953da3dca9cb4
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2022-06-17 12:19:29 +0000

    hmmm

--- assignment_no_1_cs201p.cpp
+++ assignment_no_1_cs201p.cpp
@@ -1,21 +1,30 @@
 #include <iostream>
 using namespace std;
 
-maxResult(float	eq1, float eq2, float eq3)
+maxResult(float eq1, float eq2, float eq3)
 {
 	if (eq1 > eq2 && eq1 > eq3)
 	{
-		cout << "Eqation 1: " << " " << eq1 << " " << "Result is maximum \n" << endl;
+		cout << "Eqation 1: "
+			 << " " << eq1 << " "
+			 << "Result is maximum \n"
+			 << endl;
 	}
-	 
+
 	else if (eq2 > eq1 && eq2 > eq3)
 	{
-		cout << "Eqation 2: " << " " << eq2 << " " << "Result is maximum \n" << endl;
+		cout << "Eqation 2: "
+			 << " " << eq2 << " "
+			 << "Result is maximum \n"
+			 << endl;
 	}
-	
+
 	else if (eq3 > eq1 && eq3 > eq2)
 	{
-		cout << "Equation 3: " << " " << eq3 << " " << "Result is maximum \n"<< endl;
+		cout << "Equation 3: "
+			 << " " << eq3 << " "
+			 << "Result is maximum \n"
+			 << endl;
 	}
 }
 
@@ -29,13 +38,13 @@ calculateEquationResult()
 	float c;
 	float d;
 	float x;
-	
+
 	int count = 0;
-	
+
 	while (count < 2)
 	{
 		count++;
-		
+
 		if (count == 2)
 		{
 			a = 6;
@@ -43,22 +52,28 @@ calculateEquationResult()
 			c = 8;
 			d = 9;
 			x = 11;
-		} else {
+		}
+		else
+		{
 			a = 2;
 			b = 3;
 			c = 4;
 			d = 5;
 			x = 10;
 		}
-		
-		eq1 = x + (b/(3*a));
-		eq2 = (3*a*c-b*b)/(3*a*a);
-		eq3 = (2*b*b*b - 9*(a+b+c)+27*a*a*d)/(27+a*a*a);
-		
-		cout << "Equation 1: " << " " << eq1 << endl;
-		cout << "Equation 2: " << " " << eq2 << endl;
-		cout << "Equation 3: " << " " << eq3 << "\n" << endl;
-		
+
+		eq1 = x + (b / (3 * a));
+		eq2 = (3 * a * c - b * b) / (3 * a * a);
+		eq3 = (2 * b * b * b - 9 * (a + b + c) + 27 * a * a * d) / (27 + a * a * a);
+
+		cout << "Equation 1: "
+			 << " " << eq1 << endl;
+		cout << "Equation 2: "
+			 << " " << eq2 << endl;
+		cout << "Equation 3: "
+			 << " " << eq3 << "\n"
+			 << endl;
+
 		maxResult(eq1, eq2, eq3);
 	}
 }
@@ -67,22 +82,22 @@ int main()
 {
 	char choice;
 	bool run = true;
-	
-	while(run)
+
+	while (run)
 	{
 		do
 		{
 			calculateEquationResult();
-			cout << "Do you want to perform any other calculation(y/n): ";	
+			cout << "Do you want to perform any other calculation(y/n): ";
 			cin >> choice;
 			choice = tolower(choice);
 		} while (choice != 'n' && choice != 'y');
-		
+
 		if (choice == 'n')
 		{
 			run = false;
 		}
 	}
-	
+
 	return 0;
 }
